990b3a17e279987355c2f3689e75514564521f30
dilip-4 <career.dilip.kumar@gmail.com>
2022-09-20 13:00:37 +0530

    initial

---
+++ src/test/java/org/practice/student/management/StudentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.student.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
